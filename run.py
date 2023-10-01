f3eb8006003a52d665fe0b2dbe1279acbd2687cf
jonfd4 <jonathanforsondanso@gmail.com>
2023-10-01 17:30:25 +0100

    Create get_sales_data function to get data string from user

--- run.py
+++ run.py
@@ -11,13 +11,25 @@ SCOPE = [
     The scope lists the APIs that the prohram should access in order to run. Our scope value will not change, therefore it is known as a constant variable, therefore written in capitals
     '''
 
-    #Accessing spreadsheet 
+    #Accessing spreadsheet and checking
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS= CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-sales = SHEET.worksheet('sales') # corresponds to the name of the worksheet given in google spreadsheet
 
-data = sales.get_all_values()
-print(data)
+
+''' Getting sales data from our user. Request for users to enter their data into terminal
+This will be saved as CSV- comma separated values- allows data to be saved in a table format. '''
+
+def get_sales_data():
+    '''Get sales figures input from the user'''
+    print("Please enter sales data from the last market")
+    print("Data should be six numbers, separated by commas")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f" The data provided is {data_str}")
+
+
+get_sales_data()
